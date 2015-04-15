320ee8f773b16387b6681df03bd1d118300b905c
theothermartian <sayak_123@hotmail.com>
2015-04-15 19:17:03 +0530

    Testing first commit to online repo

---
+++ xkcd.py
@@ -0,0 +1,4 @@
+#Downloads all the available xkcd comics from their archives
+#Thanks to the creator for making these free to copy and share these beautiful comics
+from bs4 import BeautifulSoup
+print "Testing commit"
